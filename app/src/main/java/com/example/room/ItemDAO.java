a4d7d2e58ed29965d1e8968db6213e422d2adfcf
luis bizinelli <luis_bizinelli@hotmail.com>
2019-10-02 19:55:40 -0300

    #3 UPDATE itemDAO.java
    update

--- app/src/main/java/com/example/room/ItemDAO.java
+++ app/src/main/java/com/example/room/ItemDAO.java
@@ -1,7 +1,17 @@
 package com.example.room;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
 
 @Dao
 public interface ItemDAO {
+
+    @Query("SELECT * FROM item")
+    List<Item> getALL();
+
+    @Insert
+    Long insert(Item item);
 }
