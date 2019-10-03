14c50eedcd26a9eb1e723f84e9abb4068a7c6cb1
luis bizinelli <luis_bizinelli@hotmail.com>
2019-10-02 21:20:29 -0300

    update eddit item

--- app/src/main/java/com/example/room/AdditemActivity.java
+++ app/src/main/java/com/example/room/AdditemActivity.java
@@ -2,13 +2,16 @@ package com.example.room;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AdditemActivity extends AppCompatActivity {
 
     private EditText editNome, editQtde;
+    private AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,5 +43,22 @@ public class AdditemActivity extends AppCompatActivity {
             editQtde.requestFocus();
             return;
         }
+        db = AppDatabase.getInstance(getApplicationContext());
+        Item item = new Item(nome, Integer.parseInt(qtde_s));
+
+        long resultado = db.itemDAO().insert(item);
+
+        if(resultado > 0){
+            Toast.makeText(this, "Item salvo!", Toast.LENGTH_SHORT).show();
+            voltar();
+        }else{
+            Toast.makeText(this, "Erro ao salvar!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void voltar() {
+        Intent intent = new Intent(AdditemActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
